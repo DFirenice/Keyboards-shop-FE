aa052e322268936627e0bd520576f18f6fd8e3d6
DFirenice <firenic.firenice@gmail.com>
2025-10-27 23:55:26 -0400

    Shopping Cart's Order Total

--- src/app/(app)/cart/page.tsx
+++ src/app/(app)/cart/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button"
 import Heading from "@/components/ui/Heading"
 import IconButton from "@/components/ui/IconButton"
+import { cn } from "@/lib/utils"
 import Image from "next/image"
 
 const cartItems = [
@@ -29,13 +31,22 @@ const cartItems = [
   }
 ]
 
+const PriceLabel = ({ label, price, isLeading }: { label: string, price: number, isLeading?: boolean }) => {
+    return (
+        <div className={cn("flex flex-row justify-between gap-2 text-sm", { "my-3 text-md": isLeading })}>
+            <span className={cn({ "font-semibold": isLeading })}>{label}</span>
+            <span className="font-semibold font-mono">${price}</span>
+        </div>
+    )
+}
+
 const ShoppingCartPage = () => {
     return (
         <section className="w-full px-12 grid place-items-center min-h-dvh">
             <div className="container mx-auto">
-                <div>
-                    <Heading size="large">Shopping Cart</Heading>
-                    <div className="w-1/2">
+                <Heading size="large">Shopping Cart</Heading>
+                <div className="flex flex-wrap flex-row *:grow justify-between gap-12">
+                    <div>
                         <ul className="*:not-last:border-b-border *:not-last:border-b-2">
                             { cartItems.map(({ id, price, image, quantity, name }) => (
                                 <div key={id} className="relative flex flex-row gap-8 items-center py-8">
@@ -56,6 +67,26 @@ const ShoppingCartPage = () => {
                                 </div>
                             )) }
                         </ul>
+                    </div>
+                    <div className="rounded-2xl border-border border-1 p-12 space-y-8">
+                        <Heading className="font-medium" size="medium">Order Summary</Heading>
+                        <div className="flex flex-col gap-2">
+                            <span>Discount code / Promo code</span>
+                            <input placeholder="Code" className="px-2 py-4 border-border border-1 rounded-md text-secondary-foreground" />
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <span>Your bonus card number</span>
+                            <input placeholder="Enter card number" className="px-2 py-4 border-border border-1 rounded-md text-secondary-foreground" />
+                        </div>
+                        <div>
+                            <PriceLabel isLeading label="Subtotal" price={2301} />
+
+                            <PriceLabel label="Estimated tax" price={50} />
+                            <PriceLabel label="Estimated shipping & Handling" price={29} />
+                            
+                            <PriceLabel isLeading label="Total" price={2938} />
+                        </div>
+                        <Button className="w-full py-6" size="lg">Checkout</Button>
                     </div>
                 </div>
             </div>
